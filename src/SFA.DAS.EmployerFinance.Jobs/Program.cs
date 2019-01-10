b9aadf2a2536e72dfb349f7f8c9d0445c2d27bb2
Robin O'Connor <github@rocketmansoftware.com>
2019-01-10 15:02:12 +0000

    Added dependancy injection and main class to jobs project

---
+++ src/SFA.DAS.EmployerFinance.Jobs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.EmployerFinance.Jobs.DependencyResolution;
+
+namespace SFA.DAS.EmployerFinance.Jobs
+{
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            using (var container = IoC.Initialize())
+            {
+            }
+        }
+    }
+}
