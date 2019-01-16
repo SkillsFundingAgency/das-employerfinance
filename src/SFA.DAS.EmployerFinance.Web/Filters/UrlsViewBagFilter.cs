b82340f86540b0a157954486dbc406131c5fe8bf
Phil Davies <github@phildavies.co.uk>
2019-01-16 13:37:16 +0000

    no need for the func now

--- src/SFA.DAS.EmployerFinance.Web/Filters/UrlsViewBagFilter.cs
+++ src/SFA.DAS.EmployerFinance.Web/Filters/UrlsViewBagFilter.cs
@@ -1,7 +1,6 @@
-using System;
-using Microsoft.AspNetCore.Mvc.Filters;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
 using SFA.DAS.EmployerFinance.Web.Urls;
 using StructureMap;
@@ -18,11 +17,8 @@ namespace SFA.DAS.EmployerFinance.Web.Filters
 
             // options: https://stackoverflow.com/questions/32459670/resolving-instances-with-asp-net-core-di
 
-            //todo: no need for func if we get it each time, rather than using ctor injection
             var container = context.HttpContext.RequestServices.GetService<IContainer>();
-            var getEmployerUrls = container.GetInstance<Func<IEmployerUrls>>();
-
-            controller.ViewData["EmployerUrls"] = getEmployerUrls();
+            controller.ViewData["EmployerUrls"] = container.GetInstance<IEmployerUrls>();
 
             await next();
         }
