eeff3f20094385e35e5a4992aaa942da9be56770
Phil Davies <github@phildavies.co.uk>
2019-01-17 08:45:52 +0000

    update SFA.DAS.AutoConfiguration package

--- src/SFA.DAS.EmployerFinance/DependencyResolution/ConfigurationRegistry.cs
+++ src/SFA.DAS.EmployerFinance/DependencyResolution/ConfigurationRegistry.cs
@@ -1,7 +1,7 @@
-using StructureMap;
 using SFA.DAS.AutoConfiguration;
 using SFA.DAS.AutoConfiguration.DependencyResolution;
 using SFA.DAS.EmployerFinance.Configuration;
+using StructureMap;
 
 namespace SFA.DAS.EmployerFinance.DependencyResolution
 {
@@ -11,7 +11,7 @@ namespace SFA.DAS.EmployerFinance.DependencyResolution
         {
             IncludeRegistry<AutoConfigurationRegistry>();
 
-            For<EmployerFinanceConfiguration>().Use(c => c.GetInstance<ITableStorageConfigurationService>().Get<EmployerFinanceConfiguration>(ConfigurationKeys.EmployerFinance)).Singleton();
+            For<EmployerFinanceConfiguration>().Use(c => c.GetInstance<IAutoConfigurationService>().Get<EmployerFinanceConfiguration>(ConfigurationKeys.EmployerFinance)).Singleton();
             For<IEmployerUrlsConfiguration>().Use(c => c.GetInstance<EmployerFinanceConfiguration>().EmployerUrls).Singleton();
             For<IOidcConfiguration>().Use(c => c.GetInstance<EmployerFinanceConfiguration>().Oidc).Singleton();
         }
