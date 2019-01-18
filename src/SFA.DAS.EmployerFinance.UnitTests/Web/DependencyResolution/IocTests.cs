ed11b4f0d80568d03b729a8d0ff1adc6aa52b738
Phil Davies <github@phildavies.co.uk>
2019-01-18 12:13:15 +0000

    add unit test for IoC

---
+++ src/SFA.DAS.EmployerFinance.UnitTests/Web/DependencyResolution/IocTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using SFA.DAS.EmployerFinance.Web.DependencyResolution;
+using StructureMap;
+
+namespace SFA.DAS.EmployerFinance.UnitTests.Web.DependencyResolution
+{
+    [TestFixture]
+    public class IocTests
+    {
+        [Test]
+        public void WhenIocIsInitializationThenContainerShouldBeValid()
+        {
+            var registry = new Registry();
+            IoC.Initialize(registry);
+            var container = new Container(registry);
+            container.AssertConfigurationIsValid();
+        }
+    }
+}
