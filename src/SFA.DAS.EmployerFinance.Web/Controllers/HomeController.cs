7fbfc0c91d4a9be210ea3f2d53990c24262a9b8c
Phil Davies <github@phildavies.co.uk>
2019-01-21 12:49:17 +0000

    remove todos only

--- src/SFA.DAS.EmployerFinance.Web/Controllers/HomeController.cs
+++ src/SFA.DAS.EmployerFinance.Web/Controllers/HomeController.cs
@@ -4,13 +4,6 @@ using SFA.DAS.EmployerFinance.Web.Models;
 
 namespace SFA.DAS.EmployerFinance.Web.Controllers
 {
-    //todo: ssl cert. auth (using new core startup?)
-    //todo: fonts issue (edge)
-    //todo: minification (https://docs.microsoft.com/en-us/aspnet/core/client-side/using-gulp?view=aspnetcore-2.2)
-    //todo: document/script setting up node modules (switch git to e:)
-    //todo: typescript (use BuildBundlerMinifier-Typescript?
-    //todo: package up standard das layout. could package up layout components in razor class library? libman/nuget hybrid?
-    
     [Route("")]
     public class HomeController : Controller
     {
