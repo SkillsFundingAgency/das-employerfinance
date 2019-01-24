ce0fdd3f6e96227295d1d96ec88574fea4d576eb
Elliot Ritchie <elliotritchie@hotmail.com>
2019-01-24 11:29:52 +0000

    Added SFA.DAS.EmployerFinance.Database project

---
+++ src/SFA.DAS.EmployerFinance.Database/Scripts/PostDeployment/SeedDevData.sql
@@ -0,0 +1,4 @@
+﻿IF (NOT EXISTS (SELECT 1 FROM [dbo].[Accounts] WHERE Id = 1))
+BEGIN
+    INSERT INTO [dbo].[Accounts] ([Id], [HashedId], [PublicHashedId], [Name], [Created]) VALUES (1, 'JRML7V', 'LDMVWV', 'Foobar', GETUTCDATE())
+END
