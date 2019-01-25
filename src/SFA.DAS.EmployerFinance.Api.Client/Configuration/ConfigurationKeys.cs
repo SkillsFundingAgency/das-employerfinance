c9989fd0db6758ed1ebad781108c42c4589b5132
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2019-01-25 13:09:56 +0000

    Added base code for API client

---
+++ src/SFA.DAS.EmployerFinance.Api.Client/Configuration/ConfigurationKeys.cs
@@ -0,0 +1,8 @@
+
+namespace SFA.DAS.EmployerFinance.Api.Client.Configuration
+{
+    public static class ConfigurationKeys
+    {
+        public const string ApiClientBase = "SFA.DAS.EmployerFinance.Api.ClientV2";
+    }
+}
