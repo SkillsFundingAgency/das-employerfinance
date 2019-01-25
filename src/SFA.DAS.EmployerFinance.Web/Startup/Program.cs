36e7246b0a7c7c38b0534dd26accc91df3d27b65
Phil Davies <github@phildavies.co.uk>
2019-01-25 09:14:39 +0000

    pick up the storage connection string from an environment variable

--- src/SFA.DAS.EmployerFinance.Web/Startup/Program.cs
+++ src/SFA.DAS.EmployerFinance.Web/Startup/Program.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using SFA.DAS.EmployerFinance.Configuration.AzureTableStorage;
 using StructureMap.AspNetCore;
 
 namespace SFA.DAS.EmployerFinance.Web.Startup
 {
+    public static class EnvironmentVariableNames
+    {
+        public const string Environment = "EnvironmentName";
+        public const string ConfigurationStorageConnectionString = "ConfigurationStorageConnectionString";
+    }
+
     public static class Program
     {
         public static void Main(string[] args)
@@ -12,18 +20,28 @@ namespace SFA.DAS.EmployerFinance.Web.Startup
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddEnvironmentVariables();
+            var environmentVariablesConfig = builder.Build();
+            var storageConnectionString = environmentVariablesConfig[EnvironmentVariableNames.ConfigurationStorageConnectionString];
+            if (string.IsNullOrWhiteSpace(storageConnectionString))
+                throw new Exception($"Hey {environmentVariablesConfig["USERNAME"]??"developer"}, you need to set the environment variable '{EnvironmentVariableNames.ConfigurationStorageConnectionString}'. Set it to a connection string pointing to a storage account containing a 'Configuration' table. See readme.md for more information.");
+                
+            //todo: pick up the environment also, and integrate it into core's environment system
+            
+            return WebHost.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((hostingContext, config) =>
-                    {
-                        // use hostingContext.HostingEnvironment.EnvironmentName?
-                        config.AddAzureTableStorageConfiguration(
-                            // connection string will be picked up from an environment variable
-                            "",
-                            "LOCAL", new[] {"SFA.DAS.EmployerFinanceV2"});
-                    })
+                {
+                    // use hostingContext.HostingEnvironment.EnvironmentName?
+                    config.AddAzureTableStorageConfiguration(
+                        storageConnectionString,
+                        "LOCAL", new[] {"SFA.DAS.EmployerFinanceV2"});
+                })
                 .UseKestrel(options => options.AddServerHeader = false)
                 .UseStartup<Startup>()
                 .UseStructureMap();
+        }
     }
 }
