92b25c4abdc2d594041b77d24ed9fe959ee35a20
Phil Davies <github@phildavies.co.uk>
2019-01-25 09:53:54 +0000

    config during web host building wip

--- src/SFA.DAS.EmployerFinance.Web/Startup/Program.cs
+++ src/SFA.DAS.EmployerFinance.Web/Startup/Program.cs
@@ -2,19 +2,16 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using SFA.DAS.EmployerFinance.Configuration;
 using SFA.DAS.EmployerFinance.Configuration.AzureTableStorage;
 using StructureMap.AspNetCore;
 
 namespace SFA.DAS.EmployerFinance.Web.Startup
 {
-    public static class EnvironmentVariableNames
-    {
-        public const string Environment = "EnvironmentName";
-        public const string ConfigurationStorageConnectionString = "ConfigurationStorageConnectionString";
-    }
-
     public static class Program
     {
+        private const string DefaultEnvironment = "LOCAL";
+
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
@@ -22,13 +19,16 @@ namespace SFA.DAS.EmployerFinance.Web.Startup
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
         {
-            var builder = new ConfigurationBuilder();
-            builder.AddEnvironmentVariables();
-            var environmentVariablesConfig = builder.Build();
+            var environmentVariablesConfig = new ConfigurationBuilder().AddEnvironmentVariables().Build();
             var storageConnectionString = environmentVariablesConfig[EnvironmentVariableNames.ConfigurationStorageConnectionString];
+
             if (string.IsNullOrWhiteSpace(storageConnectionString))
                 throw new Exception($"Hey {environmentVariablesConfig["USERNAME"]??"developer"}, you need to set the environment variable '{EnvironmentVariableNames.ConfigurationStorageConnectionString}'. Set it to a connection string pointing to a storage account containing a 'Configuration' table. See readme.md for more information.");
-                
+
+            var environmentName = environmentVariablesConfig[EnvironmentVariableNames.Environment] ?? DefaultEnvironment;
+            //todo: LOCAL / development etc. how to reconcile?
+            // only care about legacy envnames like LOCAL, for config?
+            
             //todo: pick up the environment also, and integrate it into core's environment system
             
             return WebHost.CreateDefaultBuilder(args)
@@ -37,7 +37,7 @@ namespace SFA.DAS.EmployerFinance.Web.Startup
                     // use hostingContext.HostingEnvironment.EnvironmentName?
                     config.AddAzureTableStorageConfiguration(
                         storageConnectionString,
-                        "LOCAL", new[] {"SFA.DAS.EmployerFinanceV2"});
+                        environmentName, new[] {ConfigurationKeys.EmployerFinance});
                 })
                 .UseKestrel(options => options.AddServerHeader = false)
                 .UseStartup<Startup>()
--- src/SFA.DAS.EmployerFinance/Configuration/ConfigurationKeys.cs
+++ src/SFA.DAS.EmployerFinance/Configuration/ConfigurationKeys.cs
@@ -1,5 +1,6 @@
 namespace SFA.DAS.EmployerFinance.Configuration
 {
+    //todo: rename ConfigurationSectionNames?
     public static class ConfigurationKeys
     {
         public const string EmployerFinance = "SFA.DAS.EmployerFinanceV2";
