615d4ad75a6a86bd016d45ed337e6676f51bcd58
Phil Davies <github@phildavies.co.uk>
2019-02-04 15:18:33 +0000

    comments only

--- src/SFA.DAS.EmployerFinance/Configuration/ConfigurationKeys.cs
+++ src/SFA.DAS.EmployerFinance/Configuration/ConfigurationKeys.cs
@@ -1,6 +1,8 @@
 namespace SFA.DAS.EmployerFinance.Configuration
 {
-    //todo: rename ConfigurationSectionNames?
+    /// <summary>
+    /// Used to reference specific rows in configuration table storage. Then becomes top-level section names using IConfiguration.
+    /// </summary>
     public static class ConfigurationKeys
     {
         public const string EmployerFinance = "SFA.DAS.EmployerFinanceV2";
