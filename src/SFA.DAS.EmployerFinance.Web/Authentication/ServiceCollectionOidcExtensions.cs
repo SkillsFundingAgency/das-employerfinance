761273eb9255a38c955e5293b2fc987e6a1b5686
Phil Davies <github@phildavies.co.uk>
2019-02-04 14:47:06 +0000

    tidy up

--- src/SFA.DAS.EmployerFinance.Web/Authentication/ServiceCollectionOidcExtensions.cs
+++ src/SFA.DAS.EmployerFinance.Web/Authentication/ServiceCollectionOidcExtensions.cs
@@ -7,18 +7,17 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using SFA.DAS.EmployerFinance.Configuration;
+using SFA.DAS.EmployerFinance.Web.Cookies;
 
 namespace SFA.DAS.EmployerFinance.Web.Authentication
 {
     //todo: options.ClaimActions.MapUniqueJsonKey("sub", "id");
-    //todo: move to authentication/extensions? rename ServiceCollectionOidcExtensions?
     //todo: looks might be able to play with the cookie domain, path etc. and have 1 cookie, so any sub-site auto signs out of main site! see https://docs.microsoft.com/en-us/aspnet/core/security/authentication/cookie?view=aspnetcore-2.2
     //todo: ios on safari and same-site cookies, see https://brockallen.com/2019/01/11/same-site-cookies-asp-net-core-and-external-authentication-providers/
     //looks like das-recruit followed this (or earlier guide)... https://identityserver4.readthedocs.io/en/latest/quickstarts/3_interactive_login.html
-    public static class ServiceCollectionExtensions
+    public static class ServiceCollectionOidcExtensions
     {
         //https://docs.microsoft.com/en-us/aspnet/core/security/authentication/social/additional-claims?view=aspnetcore-2.2
-        // reservations hooks into OnTokenValidated, example uses OnPostConfirmationAsync
         public static IServiceCollection AddAndConfigureAuthentication(this IServiceCollection services, IHostingEnvironment hostingEnvironment, IOidcConfiguration oidcConfig)
         {
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
@@ -36,12 +35,12 @@ namespace SFA.DAS.EmployerFinance.Web.Authentication
                 {
                     options.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.Always;
                     options.SlidingExpiration = true;
-                    options.ExpireTimeSpan = TimeSpan.FromMinutes(60); //AuthenticationConfiguration.SessionTimeoutMinutes);
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
                 }
 
-                //todo:
+                //todo: once in same branch as error handling, point to forbidden error page
                 options.AccessDeniedPath = "/Error/403";
-                options.Cookie.Name = "EmployerFinance.Web.Auth";
+                options.Cookie.Name = CookieNames.Authentication;
             })
             .AddOpenIdConnect(options =>
             {
@@ -50,16 +49,14 @@ namespace SFA.DAS.EmployerFinance.Web.Authentication
                 options.ResponseType = "code";
                 options.ClientId = oidcConfig.ClientId;
                 options.ClientSecret = oidcConfig.ClientSecret;
-                // we don't need this if we don't supply the token in the redirect url, which is good, because it causes the sign-out to blow up occasionally, due to the cookie containing the tokens getting too long. see... https://github.com/IdentityServer/IdentityServer4/issues/1764, also https://stackoverflow.com/questions/40934829/c-sharp-identity-server-bad-request-request-too-long
-                //options.SaveTokens = true;
 
-                //is this required? try with JwtSecurityTokenHandler.InboundClaimTypeMap.Clear()
                 options.ClaimActions.MapUniqueJsonKey("sub", "id");
                 
                 options.Events.OnRemoteFailure = ctx =>
                 {
                     if (ctx.Failure.Message.Contains("Correlation failed"))
                     {
+                        //todo: logging
 //                        var logger = services.BuildServiceProvider().GetRequiredService<ILoggerFactory>().CreateLogger<Startup>();
 //                        logger.LogDebug("Correlation Cookie was invalid - probably timed-out");
 
