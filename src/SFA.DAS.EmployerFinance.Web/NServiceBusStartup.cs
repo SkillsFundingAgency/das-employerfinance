fa03ec43b9c392dda3668d5f479dc61b8e8f6913
Phil Davies <github@phildavies.co.uk>
2019-02-06 13:00:44 +0000

    config wip (still issue with healthcheck)

--- src/SFA.DAS.EmployerFinance.Web/NServiceBusStartup.cs
+++ src/SFA.DAS.EmployerFinance.Web/NServiceBusStartup.cs
@@ -1,7 +1,7 @@
 using System.Data.Common;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using NServiceBus;
-using SFA.DAS.AutoConfiguration;
 using SFA.DAS.EmployerFinance.Configuration;
 using SFA.DAS.EmployerFinance.Extensions;
 using SFA.DAS.EmployerFinance.Startup;
@@ -18,21 +18,21 @@ namespace SFA.DAS.EmployerFinance.Web
     public class NServiceBusStartup : IRunAtStartup
     {
         private readonly IContainer _container;
-        private readonly IEnvironmentService _environmentService;
+        private readonly IHostingEnvironment _hostingEnvironment;
         private readonly EmployerFinanceConfiguration _employerFinanceConfiguration;
         private IEndpointInstance _endpoint;
 
-        public NServiceBusStartup(IContainer container, IEnvironmentService environmentService, EmployerFinanceConfiguration employerFinanceConfiguration)
+        public NServiceBusStartup(IContainer container, IHostingEnvironment hostingEnvironment, EmployerFinanceConfiguration employerFinanceConfiguration)
         {
             _container = container;
-            _environmentService = environmentService;
+            _hostingEnvironment = hostingEnvironment;
             _employerFinanceConfiguration = employerFinanceConfiguration;
         }
 
         public async Task StartAsync()
         {
             var endpointConfiguration = new EndpointConfiguration("SFA.DAS.EmployerFinanceV2.Web")
-                .UseAzureServiceBusTransport(() => _employerFinanceConfiguration.ServiceBusConnectionString, _environmentService.IsCurrent(DasEnv.LOCAL))
+                .UseAzureServiceBusTransport(() => _employerFinanceConfiguration.ServiceBusConnectionString, _hostingEnvironment.IsDevelopment())
                 .UseErrorQueue()
                 .UseInstallers()
                 .UseLicense(_employerFinanceConfiguration.NServiceBusLicense)
