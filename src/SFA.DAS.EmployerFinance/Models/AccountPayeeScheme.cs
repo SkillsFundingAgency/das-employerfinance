29ca0c6a4586f5d06cc7dd2fae67dfce525f9372
Phil Davies <github@phildavies.co.uk>
2019-02-18 15:09:52 +0000

    payescheme & accountpayescheme models wip

--- src/SFA.DAS.EmployerFinance/Models/AccountPayeeScheme.cs
+++ src/SFA.DAS.EmployerFinance/Models/AccountPayeeScheme.cs
@@ -1,7 +1,39 @@
+using System;
+
 namespace SFA.DAS.EmployerFinance.Models
 {
     public class AccountPayeeScheme
     {
+        public long AccountId { get; private set; }
+        public string EmployerReferenceNumber { get; private set; }
+        public DateTime Created { get; private set; }
+        public DateTime? Updated { get; private set; }
+        public DateTime? Deleted { get; private set; }
         
+        internal AccountPayeeScheme(long accountId, string employerReferenceNumber, DateTime created)
+        {
+            AccountId = accountId;
+            EmployerReferenceNumber = employerReferenceNumber;
+            Created = created;
+        }
+
+        private AccountPayeeScheme()
+        {
+        }
+
+        internal void Delete(DateTime deleted)
+        {
+            EnsureHasNotBeenDeleted();
+            
+            Deleted = deleted;
+        }
+
+        private void EnsureHasNotBeenDeleted()
+        {
+            if (Deleted != null)
+            {
+                throw new InvalidOperationException("Requires account payee scheme has not been deleted");
+            }
+        }
     }
 }
--- src/SFA.DAS.EmployerFinance/Models/PayeScheme.cs
+++ src/SFA.DAS.EmployerFinance/Models/PayeScheme.cs
@@ -1,7 +1,24 @@
+using System;
+
 namespace SFA.DAS.EmployerFinance.Models
 {
     public class PayeScheme
     {
+        public string EmployerReferenceNumber { get; private set; }
+        public string Name { get; private set; }
+        public DateTime Created { get; private set; }
+        public DateTime? Updated { get; private set; }
+        public DateTime? Deleted { get; private set; }
         
+        public PayeScheme(string employerReferenceNumber, string name, DateTime created)
+        {
+            EmployerReferenceNumber = employerReferenceNumber;
+            Name = name;
+            Created = created;
+        }
+
+        private PayeScheme()
+        {
+        }
     }
 }
