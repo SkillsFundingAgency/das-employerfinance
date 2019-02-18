d14a5ec33c8e8f27c615c17bfe86fd116a202912
Phil Davies <github@phildavies.co.uk>
2019-02-18 14:54:53 +0000

    add user model

---
+++ src/SFA.DAS.EmployerFinance/Models/User.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace SFA.DAS.EmployerFinance.Models
+{
+    public class User : Entity
+    {
+        public Guid Ref { get; private set; }
+        public string Email { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public DateTime Created { get; private set; }
+        public DateTime? Updated { get; private set; }
+
+        public User(Guid @ref, string email, string firstName, string lastName)
+        {
+            Ref = @ref;
+            Email = email;
+            FirstName = firstName;
+            LastName = lastName;
+            Created = DateTime.UtcNow;
+        }
+
+        private User()
+        {
+        }
+
+        public void Update(string email, string firstName, string lastName)
+        {
+            if (email != Email || firstName != FirstName || lastName != LastName)
+            {
+                Email = email;
+                FirstName = firstName;
+                LastName = lastName;
+                Updated = DateTime.UtcNow;
+            }
+        }
+    }
+}
