d8354fb2a0e539738c914e8caf9db3909f003687
Phil Davies <github@phildavies.co.uk>
2019-02-19 13:06:31 +0000

    PayeSchemes schema wip

--- src/SFA.DAS.EmployerFinance/Data/Scripts/Script0009CreatePayeSchemes.sql
+++ src/SFA.DAS.EmployerFinance/Data/Scripts/Script0009CreatePayeSchemes.sql
@@ -1,11 +1,11 @@
 CREATE TABLE [dbo].[PayeSchemes]
 (
-  [Id] BIGINT NOT NULL,
+  [Id] BIGINT NOT NULL IDENTITY,
   [EmployerReferenceNumber] VARCHAR(16) NOT NULL,
   [Name] NVARCHAR(60) NULL, -- todo: check source for >60
   [Created] DATETIME2 NOT NULL,
   [Updated] DATETIME2 NULL,
   [Deleted] DATETIME2 NULL,
-  CONSTRAINT [PK_PayeSchemes] PRIMARY KEY CLUSTERED ([Id] ASC),
-  CONSTRAINT [AK_PayeSchemes_EmployerReferenceNumber] UNIQUE ([EmployerReferenceNumber] ASC)
+  CONSTRAINT [PK_PayeSchemes] PRIMARY KEY NONCLUSTERED ([EmployerReferenceNumber] ASC),
+  CONSTRAINT [AK_PayeSchemes_Id] UNIQUE CLUSTERED ([Id] ASC)
 )
