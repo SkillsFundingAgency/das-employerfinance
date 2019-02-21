76b9b8aa2065a63e2aa4786ce30dc6ce17f74e70
Phil Davies <github@phildavies.co.uk>
2019-02-21 12:41:01 +0000

    initial seeding wip

--- src/SFA.DAS.EmployerFinance/Data/Scripts/Ad Hoc/SeedFromEmployerAccount.sql
+++ src/SFA.DAS.EmployerFinance/Data/Scripts/Ad Hoc/SeedFromEmployerAccount.sql
@@ -1,15 +1,15 @@
 /*
-Script to populate Account/Legal Entity data for provider relationships from MA source data
+Script to populate Account/Legal Entity data for employer finance v2 from MA source data
 
 Instructions for use:
 1. Think about obtaining a prod db backup for sourcing the data, since it kills the db
 2. Turn on SQL CMD mode
 3. Execute this script against MA employer_account database
-4. Execute the resulting script against provider-relationships database
+4. Execute the resulting script against EmployerFinanceV2 database
 
 Expectations:
 ~30 minutes on employer_account database
-~20 seconds on provider-relationships db
+~20 seconds on employer finance v2 db
 ~15k+ Accounts created
 ~27k+ AccountLegalEntities created
 */
@@ -19,14 +19,14 @@ SET NOCOUNT ON
 declare @MAXINSERT int = 1000 --insert values() batch size (cannot be more than 1000)
 
 --Some table var declarations
-print 'declare @Accounts table ([AccountId] bigint,[HashedId] nvarchar(100),[PublicHashedId] nvarchar(100),[Name] nvarchar(100),[CreatedDate] datetime)'
-print 'declare @AccountLegalEntities table ([AccountLegalEntityId] bigint,[AccountLegalEntityPublicHashedId] nvarchar(6),[AccountId] bigint,[Name] nvarchar(100),[Created] datetime)'
+print 'declare @Accounts table ([Id] bigint,[HashedId] char(6),[PublicHashedId] char(6),[Name] nvarchar(100),[Created] datetime2)'
+print 'declare @AccountLegalEntities table ([Id] bigint,[PublicHashedId] char(6),[AccountId] bigint,[Name] nvarchar(100),[Created] datetime2)'
 
 BEGIN TRY
 
   --Accounts
   select
-      case (ROW_NUMBER() OVER (ORDER BY a.Id) % @MAXINSERT) when 1 then 'insert into @Accounts ([AccountId],[HashedId],[PublicHashedId],[Name],[CreatedDate]) values' + char(13) + char(10) else '' end +
+      case (ROW_NUMBER() OVER (ORDER BY a.Id) % @MAXINSERT) when 1 then 'insert into @Accounts ([Id],[HashedId],[PublicHashedId],[Name],[Created]) values' + char(13) + char(10) else '' end +
       ' (' + convert(varchar,[Id]) + ', ' + '''' + convert(varchar,[HashedId]) + '''' + ', ' + '''' + convert(varchar,[PublicHashedId]) + '''' + ', ' + '''' + replace([Name],'''','''''') + '''' + ', ' + '''' + convert(varchar,[CreatedDate],121) + '''' + ')' +
       case when ((ROW_NUMBER() OVER (ORDER BY a.Id) % @MAXINSERT = 0) OR (ROW_NUMBER() OVER (ORDER BY a.Id) = (select count(1) from [employer_account].[Account] where HashedId is not null and PublicHashedId is not null))) then '' else ',' end
   from
@@ -38,7 +38,7 @@ BEGIN TRY
 
   --AccountLegalEntities
   select
-      case (ROW_NUMBER() OVER (ORDER BY ale.Id) % @MAXINSERT) when 1 then 'insert into @AccountLegalEntities ([AccountLegalEntityId],[AccountLegalEntityPublicHashedId],[AccountId],[Name],[Created]) values' + char(13) + char(10) else '' end +
+      case (ROW_NUMBER() OVER (ORDER BY ale.Id) % @MAXINSERT) when 1 then 'insert into @AccountLegalEntities ([Id],[PublicHashedId],[AccountId],[Name],[Created]) values' + char(13) + char(10) else '' end +
       ' (' + convert(varchar,ale.[Id]) + ', '
       + '''' + ale.[PublicHashedId] + '''' + ', '
       + convert(varchar,ale.[AccountId]) + ', '
@@ -60,15 +60,15 @@ BEGIN TRY
 	BEGIN TRANSACTION
 
 	insert into Accounts ([Id], [HashedId], [PublicHashedId], [Name], [Created])
-	select a.[AccountId], a.[HashedId], a.[PublicHashedId], a.[Name], a.[CreatedDate] from @Accounts a
-	left join Accounts e on e.[Id] = a.[AccountId]
+	select a.[Id], a.[HashedId], a.[PublicHashedId], a.[Name], a.[Created] from @Accounts a
+	left join Accounts e on e.[Id] = a.[Id]
 	where e.[Id] is null --skip existing
 	print ''Inserted '' + convert(varchar,@@ROWCOUNT) + '' Accounts''
 	'
 
   print '
 	insert into AccountLegalEntities([Id],[PublicHashedId],[AccountId],[Name], [Created])
-	select ale.[AccountLegalEntityId], ale.[AccountLegalEntityPublicHashedId], ale.[AccountId], ale.[Name], ale.[Created] 
+	select ale.[Id], ale.[PublicHashedId], ale.[AccountId], ale.[Name], ale.[Created] 
 	from @AccountLegalEntities ale
 	left join AccountLegalEntities e on e.[Id] = ale.[AccountLegalEntityId]
 	where e.[Id] is null --skip existing
@@ -81,5 +81,5 @@ BEGIN TRY
 
 END TRY
 BEGIN CATCH
-  PRINT 'Problem, there are blank public hashed Id'
+  PRINT 'Problem, (blank public hashed Id?)'
 END CATCH
