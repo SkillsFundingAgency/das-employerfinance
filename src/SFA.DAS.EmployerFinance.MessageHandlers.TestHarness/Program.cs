315eef781ff4db90406a81e0d040634575c294f5
Phil Davies <github@phildavies.co.uk>
2019-02-26 09:42:39 +0000

    add MessageHandlers test harness

---
+++ src/SFA.DAS.EmployerFinance.MessageHandlers.TestHarness/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SFA.DAS.EmployerFinance.MessageHandlers.TestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
