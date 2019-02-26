2daf40bfd7373d238adbd037fd0d55a90dda288f
Phil Davies <github@phildavies.co.uk>
2019-02-26 09:33:00 +0000

    message handlers unit tests wip

--- src/SFA.DAS.EmployerFinance.UnitTests/MessageHandlers/EmployerAccounts/AddedPayeSchemeEventHandlerTests.cs
+++ src/SFA.DAS.EmployerFinance.UnitTests/MessageHandlers/EmployerAccounts/AddedPayeSchemeEventHandlerTests.cs
@@ -1,11 +1,27 @@
+using System.Threading.Tasks;
 using NUnit.Framework;
+using SFA.DAS.EmployerAccounts.Messages.Events;
+using SFA.DAS.EmployerFinance.Application.Commands.AddAccountPayeScheme;
+using SFA.DAS.EmployerFinance.MessageHandlers.EventHandlers.EmployerAccounts;
+using SFA.DAS.Testing;
 
 namespace SFA.DAS.EmployerFinance.UnitTests.MessageHandlers.EmployerAccounts
 {
     [TestFixture]
     [Parallelizable]
-    public class AddedPayeSchemeEventHandlerTests
+    public class AddedPayeSchemeEventHandlerTests : FluentTest<AddedPayeSchemeEventHandlerTestsFixture>
     {
-        
+        [Test]
+        public Task Handle_WhenHandlingCreatedAccountEventAddedPayeSchemeEvent_ThenShouldSendAddAccountCommand()
+        {
+            return TestAsync(f => f.Handle(), f => f.VerifySend<AddAccountPayeSchemeCommand>((c, m) => 
+                c.AccountId == m.AccountId &&
+                c.EmployerReferenceNumber == m.PayeRef &&
+                c.Added == m.Created));
+        }
+    }
+
+    public class AddedPayeSchemeEventHandlerTestsFixture : EventHandlerTestsFixture<AddedPayeSchemeEvent, AddedPayeSchemeEventHandler>
+    {
     }
 }
