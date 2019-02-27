1f60bb7ed8cc3846aca7f283850f93d3c67411e3
Phil Davies <github@phildavies.co.uk>
2019-02-27 09:04:01 +0000

    model unit tests wip

--- src/SFA.DAS.EmployerFinance.UnitTests/Models/AccountTests.cs
+++ src/SFA.DAS.EmployerFinance.UnitTests/Models/AccountTests.cs
@@ -1,11 +1,45 @@
+using System;
+using AutoFixture;
+using FluentAssertions;
 using NUnit.Framework;
+using SFA.DAS.EmployerFinance.Models;
+using SFA.DAS.Testing;
 
 namespace SFA.DAS.EmployerFinance.UnitTests.Models
 {
     [TestFixture]
     [Parallelizable]
-    public class AccountTests
+    public class AccountTests : FluentTest<AccountTestsFixture>
     {
+        [Test]
+        public void UpdateName_WhenAccountPreviouslyUpdatedAndUpdateNameHasLaterUpdatedTime_ThenAccountShouldBeUpdated()
+        {
+            Test(f => f.PreviousUpdate(), f => f.Account.UpdateName(f.NewName, f.ActionDateAfterLastUpdate), f =>
+            {
+                f.Account.Name.Should().Be(f.NewName);
+                f.Account.Updated.Should().Be(f.ActionDateAfterLastUpdate);
+            });
+        }
+    }
+
+    public class AccountTestsFixture
+    {
+        public Account Account { get; set; }
+        public string NewName { get; set; }
+        public DateTime ActionDateAfterLastUpdate { get; set; }
+        public Fixture Fixture { get; set; }
         
+        public AccountTestsFixture()
+        {
+            Fixture = new Fixture();
+            Account = Fixture.Create<Account>();
+            NewName = Fixture.Create<string>();
+        }
+
+        public void PreviousUpdate()
+        {
+            Account.Updated = Fixture.Create<DateTime>();
+            ActionDateAfterLastUpdate = Account.Updated.Value.AddMinutes(1);
+        }
     }
 }
--- src/SFA.DAS.EmployerFinance/Models/Account.cs
+++ src/SFA.DAS.EmployerFinance/Models/Account.cs
@@ -6,12 +6,12 @@ namespace SFA.DAS.EmployerFinance.Models
 {
     public class Account : Entity
     {
-        public long Id { get; private set; }
-        public string HashedId { get; private set; }
-        public string PublicHashedId { get; private set; }
-        public string Name { get; private set; }
-        public DateTime Created { get; private set; }
-        public DateTime? Updated { get; private set; }
+        public long Id { get; internal set; }
+        public string HashedId { get; internal set; }
+        public string PublicHashedId { get; internal set; }
+        public string Name { get; internal set; }
+        public DateTime Created { get; internal set; }
+        public DateTime? Updated { get; internal set; }
         public IEnumerable<AccountPayeScheme> AccountPayeSchemes => _accountPayeSchemes;
         
         private readonly List<AccountPayeScheme> _accountPayeSchemes = new List<AccountPayeScheme>();
