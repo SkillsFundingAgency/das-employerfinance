ce5eb5d89b4a5ff92f5314e729207761f710aca6
Phil Davies <github@phildavies.co.uk>
2019-02-27 14:08:09 +0000

    model unit tests wip

--- src/SFA.DAS.EmployerFinance.UnitTests/Models/UserTests.cs
+++ src/SFA.DAS.EmployerFinance.UnitTests/Models/UserTests.cs
@@ -26,7 +26,7 @@ namespace SFA.DAS.EmployerFinance.UnitTests.Models
         [TestCase("email", "firstName", "lastName")]
         public void Update_WhenPropertiesBeingUpdatedAreDifferentToCurrentProperties_ThenPropertiesShouldBeUpdatesAndUpdatedTimeSet(string email, string firstName, string lastName)
         {
-            Test(f => f.UpdateDefaultingToSame(email, firstName, lastName), f => f.AssertUpdated(email, firstName, lastName));
+            Test(f => f.UpdateDefaultingToSame(email, firstName, lastName), f => f.AssertUpdatedDefaultingToOriginalValues(email, firstName, lastName));
         }
     }
 
@@ -42,11 +42,6 @@ namespace SFA.DAS.EmployerFinance.UnitTests.Models
             User = _fixture.Create<User>();
             OriginalUser = CloneUser(User);
         }
-
-//        public void Update(string email = null, string firstName = null, string lastName = null)
-//        {
-//            User.Update(email, firstName, lastName);
-//        }
         
         public User CloneUser(User user)
         {
@@ -60,7 +55,7 @@ namespace SFA.DAS.EmployerFinance.UnitTests.Models
             User.Update(email ?? User.Email, firstName ?? User.FirstName, lastName ?? User.LastName);
         }
 
-        public void AssertUpdated(string expectedEmail = null, string expectedFirstName = null, string expectedLastName = null)
+        public void AssertUpdatedDefaultingToOriginalValues(string expectedEmail = null, string expectedFirstName = null, string expectedLastName = null)
         {
             User.Email.Should().Be(expectedEmail ?? OriginalUser.Email);
             User.FirstName.Should().Be(expectedFirstName ?? OriginalUser.FirstName);
