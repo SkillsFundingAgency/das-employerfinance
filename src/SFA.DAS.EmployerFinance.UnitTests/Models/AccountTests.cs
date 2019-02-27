784bbb7d97d2e77f43a0862353914a04025396da
Phil Davies <github@phildavies.co.uk>
2019-02-27 11:13:40 +0000

    model unit tests wip

--- src/SFA.DAS.EmployerFinance.UnitTests/Models/AccountTests.cs
+++ src/SFA.DAS.EmployerFinance.UnitTests/Models/AccountTests.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json;
 using NUnit.Framework;
 using SFA.DAS.EmployerFinance.Models;
 using SFA.DAS.Testing;
+using Fix = SFA.DAS.EmployerFinance.UnitTests.Models.AccountTestsFixture;
 
 namespace SFA.DAS.EmployerFinance.UnitTests.Models
 {
@@ -15,95 +16,91 @@ namespace SFA.DAS.EmployerFinance.UnitTests.Models
         [Test]
         public void UpdateName_WhenAccountNotPreviouslyUpdated_ThenAccountShouldBeUpdated()
         {
-            Test(f => f.UpdateName(f.NewName), f =>
-            {
-                f.Account.Name.Should().Be(f.NewName);
-                f.Account.Updated.Should().Be(f.ActionDate);
-            });
+            Test(f => f.UpdateName(), f => f.AssertNameAndUpdatedDateUpdated());
         }
 
         [Test]
         public void UpdateName_WhenAccountPreviouslyUpdatedAndUpdateNameHasLaterUpdatedTime_ThenAccountShouldBeUpdated()
         {
-            Test(f => f.UpdatedPreviously(), f => f.UpdateName(f.NewName), f =>
-            {
-                f.Account.Name.Should().Be(f.NewName);
-                f.Account.Updated.Should().Be(f.ActionDate);
-            });
+            Test(f => f.UpdatedPreviously(), f => f.UpdateName(), f => f.AssertNameAndUpdatedDateUpdated());
         }
         
         [Test]
         public void UpdateName_WhenAccountPreviouslyUpdatedAndUpdateNameHasEarlierUpdatedTime_ThenAccountShouldNotBeUpdated()
         {
-            Test(f => f.UpdatedWithFutureUpdate().CloneOriginalAccount(), f => f.UpdateName(f.NewName), f =>
-            {
-                f.Account.Name.Should().Be(f.OriginalAccount.Name);
-                f.Account.Updated.Should().Be(f.OriginalAccount.Updated);
-            });
+            Test(f => f.UpdatedWithFutureUpdate().CloneOriginalAccount(), f => f.UpdateName(),
+                f => f.AssertNameAndUpdatedDateNotUpdated());
         }
         
         [Test]
         public void UpdateName_WhenAccountNotPreviouslyUpdatedAndNewNameIsSameAsOldName_ThenAccountShouldNotBeUpdated()
         {
-            Test(f => f.CloneOriginalAccount(),f => f.UpdateName(f.Account.Name), f =>
-            {
-                f.Account.Name.Should().Be(f.OriginalAccount.Name);
-                f.Account.Updated.Should().Be(f.OriginalAccount.Updated);
-            });
+            Test(f => f.CloneOriginalAccount(),f => f.UpdateName(f.Account.Name),
+                f => f.AssertNameAndUpdatedDateNotUpdated());
         }
 
         [Test]
         public void UpdateName_WhenAccountPreviouslyUpdatedAndUpdateNameHasLaterUpdatedTimeAndNewNameIsSameAsOldName_ThenAccountShouldNotBeUpdated()
         {
-            Test(f => f.UpdatedPreviously().CloneOriginalAccount(), f => f.UpdateName(f.Account.Name), f =>
-            {
-                f.Account.Name.Should().Be(f.OriginalAccount.Name);
-                f.Account.Updated.Should().Be(f.OriginalAccount.Updated);
-            });
+            Test(f => f.UpdatedPreviously().CloneOriginalAccount(), f => f.UpdateName(f.Account.Name),
+                f => f.AssertNameAndUpdatedDateNotUpdated());
         }
         
         [Test]
         public void UpdateName_WhenAccountPreviouslyUpdatedAndUpdateNameHasEarlierUpdatedTimeAndNewNameIsSameAsOldName_ThenAccountShouldNotBeUpdated()
         {
-            Test(f => f.UpdatedWithFutureUpdate().CloneOriginalAccount(), f => f.UpdateName(f.Account.Name), f =>
-            {
-                f.Account.Name.Should().Be(f.OriginalAccount.Name);
-                f.Account.Updated.Should().Be(f.OriginalAccount.Updated);
-            });
+            Test(f => f.UpdatedWithFutureUpdate().CloneOriginalAccount(), f => f.UpdateName(f.Account.Name),
+                f => f.AssertNameAndUpdatedDateNotUpdated());
         }
+
+        //todo: always clone before act?
+        [Test]
+        public void AddPayeScheme_WhenPayeSchemeIsNewToTheAccount_ThenPayeSchemeShouldBeAddedToTheAccount()
+        {
+            Test(f => f.CloneOriginalAccount(), f => f.AddPayeScheme(), f => f.AssertPayeSchemeAdded());
+        }
+        
+//        [Test]
+//        public void AddPayeScheme_WhenPayeSchemeIsNewToTheAccount_ThenPayeSchemeShouldBeReturned()
+//        {
+//            Test(f => f.AddPayeScheme(), f => f.);
+//        }
+//
+//        [Test]
+//        public void AddPayeScheme_WhenPayeSchemeIsNewToTheAccount_ThenPayeSchemeAddedToTheAccountShouldBeSameAsPayeSchemeReturned()
+//        {
+//            Test(f => f.AddPayeScheme(), f => f.);
+//        }
     }
 
     public class AccountTestsFixture
     {
         public Account Account { get; set; }
         public Account OriginalAccount { get; set; }
-        public string NewName { get; set; }
+        public const string NewName = "NewName";
+        public const string EmployerReferenceNumber = "ABC/123456";
         public DateTime ActionDate { get; set; }
-        public Fixture Fixture { get; set; }
+        private readonly Fixture _fixture;
+        
+        #region Arrange
         
         public AccountTestsFixture()
         {
-            Fixture = new Fixture();
-            Account = Fixture.Create<Account>();
-            ActionDate = Fixture.Create<DateTime>();
-            NewName = Fixture.Create<string>();
-        }
-
-        public void UpdateName(string name)
-        {
-            Account.UpdateName(name, ActionDate);
+            _fixture = new Fixture();
+            Account = _fixture.Create<Account>();
+            ActionDate = _fixture.Create<DateTime>();
         }
         
         public AccountTestsFixture UpdatedPreviously()
         {
-            Account.Updated = Fixture.Create<DateTime>();
+            Account.Updated = _fixture.Create<DateTime>();
             ActionDate = Account.Updated.Value.AddMinutes(1);
             return this;
         }
 
         public AccountTestsFixture UpdatedWithFutureUpdate()
         {
-            Account.Updated = Fixture.Create<DateTime>();
+            Account.Updated = _fixture.Create<DateTime>();
             ActionDate = Account.Updated.Value.AddMinutes(-1);
             return this;
         }
@@ -114,5 +111,42 @@ namespace SFA.DAS.EmployerFinance.UnitTests.Models
             OriginalAccount.Updated = Account.Updated;
             return this;
         }
+
+        #endregion Arrange
+
+        #region Act
+        
+        public void UpdateName(string name = NewName)
+        {
+            Account.UpdateName(name, ActionDate);
+        }
+
+        public void AddPayeScheme(string employerReferenceNumber = EmployerReferenceNumber)
+        {
+            Account.AddPayeScheme(employerReferenceNumber, ActionDate);
+        }
+        
+        #endregion Act
+
+        #region Assert
+
+        public void AssertNameAndUpdatedDateUpdated()
+        {
+            Account.Name.Should().Be(NewName);
+            Account.Updated.Should().Be(ActionDate);
+        }
+
+        public void AssertNameAndUpdatedDateNotUpdated()
+        {
+            Account.Name.Should().Be(OriginalAccount.Name);
+            Account.Updated.Should().Be(OriginalAccount.Updated);
+        }
+
+        public void AssertPayeSchemeAdded()
+        {
+            Account.AccountPayeSchemes.Should().BeEquivalentTo(new AccountPayeScheme(OriginalAccount.Id, EmployerReferenceNumber, ActionDate));
+        }
+        
+        #endregion Assert
     }
 }
