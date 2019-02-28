21ebb0ebd1eba8a865ceb371b94355dcd757cbd4
Phil Davies <github@phildavies.co.uk>
2019-02-28 11:22:57 +0000

    Levy/Transaction schema wip

--- src/SFA.DAS.EmployerFinance/Data/Scripts/Script0011CreateTransactions.sql
+++ src/SFA.DAS.EmployerFinance/Data/Scripts/Script0011CreateTransactions.sql
@@ -4,10 +4,50 @@ CREATE TABLE [dbo].[Transactions]
   [AccountId] BIGINT NOT NULL,
   [Type] TINYINT NOT NULL,
   [SubType] TINYINT NOT NULL,
+  --payroll month 1 is 6 apr -> 5 may (tax year)
+  --payments r01 is august (academic year)
+  --return period in payments table 1-14?
+  --tax month in levy dec 1-12?
+  --year/month or date? probably date in transactions
   [Period] DATE NOT NULL,
   [Amount] MONEY,
   [Balance] MONEY,
   [JobId] UNIQUEIDENTIFIER,
   CONSTRAINT [PK_Transactions] PRIMARY KEY CLUSTERED ([Id] ASC),
-  CONSTRAINT [FK_Transactions_Accounts_Id] FOREIGN KEY ([AccountId]) REFERENCES [Accounts] ([Id]),
+  CONSTRAINT [FK_Transactions_Accounts_Id] FOREIGN KEY ([AccountId]) REFERENCES [Accounts] ([Id])
 )
+
+/* 
+existing...
+
+CREATE TABLE [employer_financial].[TransactionLine]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    AccountId BIGINT NOT NULL,
+    DateCreated DATETIME NOT NULL,
+    SubmissionId BIGINT NULL,
+    TransactionDate DATETIME NOT NULL,
+    TransactionType TINYINT NOT NULL DEFAULT 0, 
+    LevyDeclared DECIMAL(18,4) NULL, 
+    Amount DECIMAL(18,4) NOT NULL DEFAULT 0, 
+    EmpRef nVarchar(50) null,
+    PeriodEnd nVarchar(50) null,
+    Ukprn BIGINT null, 
+    SfaCoInvestmentAmount DECIMAL(18, 4) NOT NULL DEFAULT 0, 
+    EmployerCoInvestmentAmount DECIMAL(18, 4) NOT NULL DEFAULT 0,
+    [EnglishFraction] DECIMAL(18, 5) NULL, 
+    [TransferSenderAccountId] BIGINT NULL, 
+    [TransferSenderAccountName] NVARCHAR(100) NULL,
+    [TransferReceiverAccountId] BIGINT NULL, 
+    [TransferReceiverAccountName] NVARCHAR(100) NULL
+)
+
+also, type lookup.. (required?)
+
+CREATE TABLE [employer_financial].[TransactionLineTypes]
+(	
+    [TransactionType] TINYINT NOT NULL , 
+    [Description] VARCHAR(100) NOT NULL, 
+    CONSTRAINT [PK_TransactionLineTypes] PRIMARY KEY ([TransactionType]) 
+)
+ */
--- src/SFA.DAS.EmployerFinance/Data/Scripts/Script0012LevyDeclarations.sql
+++ src/SFA.DAS.EmployerFinance/Data/Scripts/Script0012LevyDeclarations.sql
@@ -3,8 +3,34 @@ CREATE TABLE [dbo].[LevyDeclarations]
   [Id] BIGINT NOT NULL IDENTITY,
   [TransactionId] BIGINT NOT NULL,
   [HmrcSubmissionId] INT NOT NULL, ---todo: check type
-  [YearToDate] MONEY NOT NULL,
+  [SubmissionDate] DATETIME2 NOT NULL,
+  [EmployerReferenceNumber] VARCHAR(16) NOT NULL,
+  [LevyDueYearToDate] MONEY NOT NULL, -- use decimal to reduce space requirements?
   CONSTRAINT [PK_LevyDeclarations] PRIMARY KEY CLUSTERED ([Id] ASC),
   CONSTRAINT [FK_LevyDeclarations_Transactions_Id] FOREIGN KEY ([TransactionId]) REFERENCES [Transactions] ([Id]),
   CONSTRAINT [UK_LevyDeclarations_HmrcSubmissionId] UNIQUE ([HmrcSubmissionId] ASC)
 )
+
+--existing..
+/*
+CREATE TABLE [employer_financial].[LevyDeclaration]
+(
+  [Id] BIGINT NOT NULL PRIMARY KEY IDENTITY,
+  [AccountId] BIGINT NOT NULL DEFAULT 0,
+  [EmpRef] NVARCHAR(50) NOT NULL,
+  [LevyDueYTD] DECIMAL(18, 4) NULL DEFAULT 0,
+  [LevyAllowanceForYear] DECIMAL(18, 4) NULL DEFAULT 0,
+  [SubmissionDate] DATETIME NULL,
+  [SubmissionId] BIGINT NOT NULL DEFAULT 0,       -- think this is the original hmrc non-unique id. do we need it now that we have the unique HmrcSubmissionId
+  [PayrollYear] NVARCHAR(10) NULL,                -- do we want, for ease of display?
+  [PayrollMonth] TINYINT NULL,                    -- think this will be useful, even though we could calculate this from the period column in transactions table
+  [CreatedDate] DATETIME NOT NULL DEFAULT GetDate(),
+  [EndOfYearAdjustment] BIT NOT NULL DEFAULT 0,
+  [EndOfYearAdjustmentAmount] DECIMAL(18,4) NULL,   -- is this separate amount from amount, or can we just re-use amount?
+  [DateCeased] DATETIME NULL,
+  [InactiveFrom] DATETIME NULL,
+  [InactiveTo] DATETIME NULL,
+  [HmrcSubmissionId] BIGINT NULL,
+  [NoPaymentForPeriod] BIT DEFAULT 0
+)
+*/
