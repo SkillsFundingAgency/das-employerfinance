b33e1368fc65f8a0cee52917ec1771e884b91fae
Phil Davies <github@phildavies.co.uk>
2019-02-28 13:02:56 +0000

    Levy/Transaction schema wip

--- src/SFA.DAS.EmployerFinance/Data/Scripts/Script0012LevyDeclarations.sql
+++ src/SFA.DAS.EmployerFinance/Data/Scripts/Script0012LevyDeclarations.sql
@@ -73,3 +73,55 @@ declarations            array   optional
     id                  number  optional	A unique identifier for the declaration. This will remain consistent from one call to the API to the next so that the client can identify declarations they’ve already retrieved. It is the identifier assigned by the RTI system to the EPS return, so it is possible to cross-reference with HMRC if needed. Dividing this identifier by 10 (ignoring the remainder) gives the identifier assigned by the RTI system to the EPS return, so it is possible to cross-reference with HMRC if needed. Taking this identifier modulo 10 gives the type of entry: 0, no entry; 1, inactive; 2, levy declaration; 3, ceased. For example: 12345684.
     levyDueYTD          number  optional	The amount of apprenticeship levy that was declared in the payroll month. For example: 600.20.
  */
+
+/*
+Levy Table..
+
+Fields	Definition 	Source	Note
+Account ID 	Identity of the employer account	MA	
+empRef	Reference number of the PAYE scheme	MA	
+ID	A unique identifier for the declaration. 	HMRC	
+Submission ID 	HMRC ascribed submission ID	HMRC	
+Submission date time 	Date and time of the submission by the employer to HMRC	HMRC	
+PayrollYear	Financial year eg 17-18	HMRC	
+Payroll Month	The period the PAYE submission releates to 	HMRC	
+End of year adjustment	Identifies an end of year adjustment 	HMRC	
+EndOfYearAdjustmentAmount	YTD value for the month 12 	HMRC	
+LevyDueYTD 	The value of the PAYE schemes pay bill month for that financial year April / March	MA	
+English % 	The English % of the pay bill that is paid to English residents	MA	New  - not sure why the EF is not in the current Levy table
+CreatedDate	Date Your funds enter into the account	MA	
+DateCeased	Date a PAYE is ceased	HMRC	
+InactiveFrom	Indicates the the payroll scheme will be inactive starting from this date. Should always be the 6th of the month of the first inactive payroll period.	HMRC	
+InactiveTo	The date after which the payroll scheme will be active again. Should always be the 5th of the month of the last inactive payroll period	HMRC	
+HmrcSubmissionId	Same as Submission ID	HMRC	
+NoPaymentForPeriod	If present, will always have the value true and indicates that no declaration was necessary for this period. This can be interpreted to mean that the YTD levy balance is unchanged from the previous submitted value.	HMRC	
+LevyAllowanceForYear	The value of the levy allowance below which you do not paye the levy - currently £15,000	HMRC	
+
+ */
+ 
+ /*
+ Transactions table..
+ 
+ Fields	Definition 	Source	
+Id	HMRC unique identifier for the submission	HMRC	
+AccountId	Identifier of the employers account	MA	
+DateCreated	Date entered into the acocunt	MA	
+SubmissionId	Same as HMRC submissionID	HMRC	
+TransactionDate	Same as submission date	MA	
+TransactionType	Tbc	MA	
+LevyDeclared	Monthly amount declared 	MA	
+EnglishFraction	The English % of the pay bill that is paid to English residents	MA	
+English Levy	The Levy declared after multiplication with the English percentage. 	MA	New
+Top up 	10% of the value of the Levy after EF	MA	New
+Amount	The amount entering the account after the EF and 10% top up "Your funds" in UI.	MA	
+Status 	Whether a submission is Accepted / Superseded / Late	MA	New
+EmpRef	PAYE reference code	MA	
+PeriodEnd		MA	
+Ukprn	Provider Identifier	MA	
+SfaCoInvestmentAmount	Amount or provider payment funded by government when there's no levy	MA	
+EmployerCoInvestmentAmount	Amount or provider payment funded by the employer when there's no levy	MA	
+TransferSenderAccountId	Identifier of the sending employer	MA	
+TransferSenderAccountName	Name of the sending employer's account	MA	
+TransferReceiverAccountId	Identifier of the receiving employer	MA	
+TransferReceiverAccountName	Name of the receiving employer's account	MA	
+  */
