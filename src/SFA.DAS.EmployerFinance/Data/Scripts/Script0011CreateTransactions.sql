f5458199843ec6f61b1fa786fcfef78b124a9505
Phil Davies <github@phildavies.co.uk>
2019-02-28 09:28:07 +0000

    Levy/Transaction schema wip

--- src/SFA.DAS.EmployerFinance/Data/Scripts/Script0011CreateTransactions.sql
+++ src/SFA.DAS.EmployerFinance/Data/Scripts/Script0011CreateTransactions.sql
@@ -1,11 +1,13 @@
 CREATE TABLE [dbo].[Transactions]
 (
   [Id] BIGINT NOT NULL IDENTITY,
+  [AccountId] BIGINT NOT NULL,
   [Type] TINYINT NOT NULL,
   [SubType] TINYINT NOT NULL,
   [Period] DATE NOT NULL,
   [Amount] MONEY,
   [Balance] MONEY,
   [JobId] UNIQUEIDENTIFIER,
-  CONSTRAINT [PK_Transactions] PRIMARY KEY CLUSTERED ([Id] ASC)
+  CONSTRAINT [PK_Transactions] PRIMARY KEY CLUSTERED ([Id] ASC),
+  CONSTRAINT [FK_Transactions_Accounts_Id] FOREIGN KEY ([AccountId]) REFERENCES [Accounts] ([Id]),
 )
