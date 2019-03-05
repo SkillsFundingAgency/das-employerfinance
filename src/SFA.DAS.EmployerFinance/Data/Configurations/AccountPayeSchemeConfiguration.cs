555fbce4205c9f037127ea73762cb3393dd1e6c1
Elliot Ritchie <elliotritchie@hotmail.com>
2019-03-05 22:05:34 +0000

    Updated EF model to automatically filter deleted account paye schemes

--- src/SFA.DAS.EmployerFinance/Data/Configurations/AccountPayeSchemeConfiguration.cs
+++ src/SFA.DAS.EmployerFinance/Data/Configurations/AccountPayeSchemeConfiguration.cs
@@ -10,6 +10,7 @@ namespace SFA.DAS.EmployerFinance.Data.Configurations
         {
             builder.Property(aps => aps.EmployerReferenceNumber).IsRequired().HasColumnType("varchar(16)");
             builder.HasOne(aps => aps.Account).WithMany(a => a.AccountPayeSchemes).Metadata.DeleteBehavior = DeleteBehavior.Restrict;
+            builder.HasQueryFilter(aps => aps.Deleted == null);
         }
     }
 }
