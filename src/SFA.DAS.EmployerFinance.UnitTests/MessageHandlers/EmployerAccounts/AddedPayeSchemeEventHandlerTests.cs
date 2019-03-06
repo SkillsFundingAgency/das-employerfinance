cc9e26b5d6f25794dd2ac085d6984dbcfd360f51
Phil Davies <github@phildavies.co.uk>
2019-03-06 10:59:57 +0000

    third time's the charm

--- src/SFA.DAS.EmployerFinance.UnitTests/MessageHandlers/EmployerAccounts/AddedPayeSchemeEventHandlerTests.cs
+++ src/SFA.DAS.EmployerFinance.UnitTests/MessageHandlers/EmployerAccounts/AddedPayeSchemeEventHandlerTests.cs
@@ -12,7 +12,7 @@ namespace SFA.DAS.EmployerFinance.UnitTests.MessageHandlers.EmployerAccounts
     public class AddedPayeSchemeEventHandlerTests : FluentTest<AddedPayeSchemeEventHandlerTestsFixture>
     {
         [Test]
-        public Task Handle_WhenHandlingCreatedAccountEventAddedPayeSchemeEvent_ThenShouldSendAddAccountPayeSchemeCommand()
+        public Task Handle_WhenHandlingAddedPayeSchemeEvent_ThenShouldSendAddAccountPayeSchemeCommand()
         {
             return TestAsync(f => f.Handle(), f => f.VerifySend<AddAccountPayeSchemeCommand>((c, m) => 
                 c.AccountId == m.AccountId &&
