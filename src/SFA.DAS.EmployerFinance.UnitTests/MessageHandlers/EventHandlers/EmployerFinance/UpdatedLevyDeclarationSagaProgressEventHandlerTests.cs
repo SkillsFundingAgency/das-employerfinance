6046ca8907793bb5203f7396a39a97dbddeb762d
Phil Davies <github@phildavies.co.uk>
2019-03-08 14:48:13 +0000

    unit tests wip

--- src/SFA.DAS.EmployerFinance.UnitTests/MessageHandlers/EventHandlers/EmployerFinance/UpdatedLevyDeclarationSagaProgressEventHandlerTests.cs
+++ src/SFA.DAS.EmployerFinance.UnitTests/MessageHandlers/EventHandlers/EmployerFinance/UpdatedLevyDeclarationSagaProgressEventHandlerTests.cs
@@ -1,8 +1,4 @@
-using System.Threading;
 using System.Threading.Tasks;
-using MediatR;
-using Moq;
-using NServiceBus;
 using NUnit.Framework;
 using SFA.DAS.EmployerFinance.Application.Commands.UpdateLevyDeclarationTransactionBalances;
 using SFA.DAS.EmployerFinance.MessageHandlers.EventHandlers.EmployerFinance;
@@ -18,28 +14,11 @@ namespace SFA.DAS.EmployerFinance.UnitTests.MessageHandlers.EventHandlers.Employ
         [Test]
         public Task Handle_WhenHandlingEvent_ThenShouldSendCommand()
         {
-            return TestAsync(f => f.Handle(),f => f.Mediator.Verify(m => m.Send(
-                It.Is<UpdateLevyDeclarationTransactionBalancesCommand>(c => c.SagaId == f.Event.SagaId),
-                CancellationToken.None), Times.Once));
+            return TestAsync(f => f.Handle(), f => f.VerifySend<UpdateLevyDeclarationTransactionBalancesCommand>((c, m) => c.SagaId == m.SagaId));
         }
     }
 
-    public class UpdatedLevyDeclarationSagaProgressEventHandlerTestsFixture
+    public class UpdatedLevyDeclarationSagaProgressEventHandlerTestsFixture : EventHandlerTestsFixture<UpdatedLevyDeclarationSagaProgressEvent, UpdatedLevyDeclarationSagaProgressEventHandler>
     {
-        public UpdatedLevyDeclarationSagaProgressEvent Event { get; set; }
-        public Mock<IMediator> Mediator { get; set; }
-        public IHandleMessages<UpdatedLevyDeclarationSagaProgressEvent> Handler { get; set; }
-
-        public UpdatedLevyDeclarationSagaProgressEventHandlerTestsFixture()
-        {
-            Event = new UpdatedLevyDeclarationSagaProgressEvent(1);
-            Mediator = new Mock<IMediator>();
-            Handler = new UpdatedLevyDeclarationSagaProgressEventHandler(Mediator.Object);
-        }
-
-        public Task Handle()
-        {
-            return Handler.Handle(Event, null);
-        }
     }
 }
