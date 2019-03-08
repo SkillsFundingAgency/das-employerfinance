7e33a7012e7a8f090db17d2f8b54b90aa1c59d4a
Phil Davies <github@phildavies.co.uk>
2019-03-08 15:25:53 +0000

    unit tests wip

--- src/SFA.DAS.EmployerFinance.UnitTests/MessageHandlers/CommandHandlers/UpdateLevyDeclarationSagaProgressCommandHandlerTests.cs
+++ src/SFA.DAS.EmployerFinance.UnitTests/MessageHandlers/CommandHandlers/UpdateLevyDeclarationSagaProgressCommandHandlerTests.cs
@@ -1,8 +1,4 @@
-using System.Threading;
 using System.Threading.Tasks;
-using MediatR;
-using Moq;
-using NServiceBus;
 using NUnit.Framework;
 using SFA.DAS.EmployerFinance.Application.Commands.UpdateLevyDeclarationSagaProgress;
 using SFA.DAS.Testing;
@@ -17,26 +13,11 @@ namespace SFA.DAS.EmployerFinance.UnitTests.MessageHandlers.CommandHandlers
         [Test]
         public Task Handle_WhenHandlingCommand_ThenShouldSendCommand()
         {
-            return TestAsync(f => f.Handle(),f => f.Mediator.Verify(m => m.Send(f.Command, CancellationToken.None), Times.Once));
+            return TestAsync(f => f.Handle(),f => f.VerifySend());
         }
     }
 
-    public class UpdateLevyDeclarationSagaProgressCommandHandlerTestsFixture
+    public class UpdateLevyDeclarationSagaProgressCommandHandlerTestsFixture : CommandHandlerTestFixture<UpdateLevyDeclarationSagaProgressCommand, UpdateLevyDeclarationSagaProgressCommandHandler>
     {
-        public UpdateLevyDeclarationSagaProgressCommand Command { get; set; }
-        public Mock<IMediator> Mediator { get; set; }
-        public IHandleMessages<UpdateLevyDeclarationSagaProgressCommand> Handler { get; set; }
-
-        public UpdateLevyDeclarationSagaProgressCommandHandlerTestsFixture()
-        {
-            Command = new UpdateLevyDeclarationSagaProgressCommand(1);
-            Mediator = new Mock<IMediator>();
-            Handler = new UpdateLevyDeclarationSagaProgressCommandHandler(Mediator.Object);
-        }
-
-        public Task Handle()
-        {
-            return Handler.Handle(Command, null);
-        }
     }
 }
