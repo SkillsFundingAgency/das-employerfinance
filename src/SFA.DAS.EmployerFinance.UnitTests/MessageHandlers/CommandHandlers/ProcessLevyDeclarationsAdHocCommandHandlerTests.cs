375f25b4d96d04dcede79f67cc9669f71f433454
Phil Davies <github@phildavies.co.uk>
2019-03-08 15:20:26 +0000

    unit tests wip

--- src/SFA.DAS.EmployerFinance.UnitTests/MessageHandlers/CommandHandlers/ProcessLevyDeclarationsAdHocCommandHandlerTests.cs
+++ src/SFA.DAS.EmployerFinance.UnitTests/MessageHandlers/CommandHandlers/ProcessLevyDeclarationsAdHocCommandHandlerTests.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Threading;
 using System.Threading.Tasks;
-using MediatR;
-using Moq;
-using NServiceBus;
 using NUnit.Framework;
 using SFA.DAS.EmployerFinance.Application.Commands.ProcessLevyDeclarationsAdHoc;
 using SFA.DAS.Testing;
@@ -18,26 +13,11 @@ namespace SFA.DAS.EmployerFinance.UnitTests.MessageHandlers.CommandHandlers
         [Test]
         public Task Handle_WhenHandlingCommand_ThenShouldSendCommand()
         {
-            return TestAsync(f => f.Handle(),f => f.Mediator.Verify(m => m.Send(f.Command, CancellationToken.None), Times.Once));
+            return TestAsync(f => f.Handle(),f => f.VerifySend());
         }
     }
 
-    public class ProcessLevyDeclarationsAdHocCommandHandlerTestsFixture
+    public class ProcessLevyDeclarationsAdHocCommandHandlerTestsFixture : CommandHandlerTestFixture<ProcessLevyDeclarationsAdHocCommand, ProcessLevyDeclarationsAdHocCommandHandler>
     {
-        public ProcessLevyDeclarationsAdHocCommand Command { get; set; }
-        public Mock<IMediator> Mediator { get; set; }
-        public IHandleMessages<ProcessLevyDeclarationsAdHocCommand> Handler { get; set; }
-
-        public ProcessLevyDeclarationsAdHocCommandHandlerTestsFixture()
-        {
-            Command = new ProcessLevyDeclarationsAdHocCommand(DateTime.UtcNow, 1);
-            Mediator = new Mock<IMediator>();
-            Handler = new ProcessLevyDeclarationsAdHocCommandHandler(Mediator.Object);
-        }
-
-        public Task Handle()
-        {
-            return Handler.Handle(Command, null);
-        }
     }
 }
